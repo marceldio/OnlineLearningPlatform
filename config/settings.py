d02ade13e9b7f082393d2ad3e4914fe00ebd5ee3
md <marceldiomare@gmail.com>
2024-09-14 20:48:58 +0300

    task 24.1 ready

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+
 from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT
 from dotenv import load_dotenv
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- lms/models.py
+++ lms/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
 class Course(models.Model):
     title = models.CharField(max_length=200, verbose_name="Название курса")
     description = models.TextField(verbose_name="Описание курса", blank=True, null=True)
-    preview = models.ImageField(upload_to="lms/course", verbose_name="Превью", blank=True, null=True)
+    preview = models.ImageField(
+        upload_to="lms/course", verbose_name="Превью", blank=True, null=True
+    )
 
     class Meta:
         verbose_name = "Курс"
@@ -14,8 +17,12 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name="Курс", on_delete=models.CASCADE)
     title = models.CharField(max_length=200, verbose_name="Название урока")
     description = models.TextField(verbose_name="Описание урока", blank=True, null=True)
-    preview = models.ImageField(upload_to="lms/lesson", verbose_name="Превью", blank=True, null=True)
-    video = models.FileField(upload_to="lms/videos", verbose_name="Видео", blank=True, null=True)
+    preview = models.ImageField(
+        upload_to="lms/lesson", verbose_name="Превью", blank=True, null=True
+    )
+    video = models.FileField(
+        upload_to="lms/videos", verbose_name="Видео", blank=True, null=True
+    )
 
     class Meta:
         verbose_name = "Урок"
